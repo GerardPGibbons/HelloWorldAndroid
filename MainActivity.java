52c3be76429b46cec0643c4bc4a638dc81e674fc
Gerard Patrick Gibbons <31993582+GerardPGibbons@users.noreply.github.com>
2019-09-18 09:47:52 -0400

    Add files via upload
    added comment.

--- MainActivity.java
+++ MainActivity.java
@@ -13,16 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Button button = findViewById(R.id.button);
         View.OnClickListener click = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 TextView text = findViewById((R.id.textView));
                 text.setText(("You have clicked the button"));
             }
         };
     };
 }
+//commit 2
 
 
 
